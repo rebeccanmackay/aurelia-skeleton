639e54ceed22b8f2a537c7c76301c3c648feafc5
= <=>
2016-02-01 18:18:59 +0000

    Shuffle components

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import 'bootstrap';
 export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
-    .developmentLogging();
+    .developmentLogging()
+    .feature('resources/components');
 
   //Uncomment the line below to enable animation.
   //aurelia.use.plugin('aurelia-animator-css');
