c9a01514da5892d5be0260c3ed22829a2e8e7c8e
= <=>
2016-02-02 20:21:58 +0000

    Refactor nav-bar component css class

--- src/resources/components/nav-bar/index.js
+++ src/resources/components/nav-bar/index.js
@@ -1,25 +1,6 @@
 import {bindable} from 'aurelia-framework';
-import {computedFrom} from 'aurelia-framework';
 
 export class NavBar {
   @bindable router = null;
-  @bindable navType = null;
-
-  @computedFrom('navType')
-  get navClass () {
-    let cssClass = null;
-
-    switch (this.navType) {
-      case 'SECONDARY':
-        cssClass = 'nav-pills';
-        break;
-      case 'PRIMARY':
-      default:
-        cssClass = 'navbar-nav';
-        break;
-    }
-
-    return cssClass;
-  }
-
+  @bindable cssClass = null;
 }
